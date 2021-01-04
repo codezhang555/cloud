59ae58fdb68850c00a3c4dffe67c19381c9eb289
codezhang <729912142@qq.com>
2021-01-04 22:37:38 +0800

    hystrix通过注解方式请求合并

--- hystrix/src/main/java/cn/itz/cloud/controller/HelloController.java
+++ hystrix/src/main/java/cn/itz/cloud/controller/HelloController.java
@@ -117,4 +117,26 @@ public class HelloController {
     System.out.println(u5);
     ctx.close();
   }
+
+  @GetMapping("/hello6")
+  public void hello6() throws ExecutionException, InterruptedException {
+    HystrixRequestContext ctx = HystrixRequestContext.initializeContext();
+    Future<User> q1 = userService.getUserById(99);
+    Future<User> q2 = userService.getUserById(98);
+    Future<User> q3 = userService.getUserById(97);
+    Future<User> q4 = userService.getUserById(96);
+    User u1 = q1.get();
+    User u2 = q2.get();
+    User u3 = q3.get();
+    User u4 = q4.get();
+    System.out.println(u1);
+    System.out.println(u2);
+    System.out.println(u3);
+    System.out.println(u4);
+    Thread.sleep(2000 );
+    Future<User> q5 = userService.getUserById(95);
+    User u5 = q5.get();
+    System.out.println(u5);
+    ctx.close();
+  }
 }
--- hystrix/src/main/java/cn/itz/cloud/service/UserService.java
+++ hystrix/src/main/java/cn/itz/cloud/service/UserService.java
@@ -1,6 +1,9 @@
 package cn.itz.cloud.service;
 
 import cn.itz.cloud.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Future;
 
 /**
  * @author CodeZhang
@@ -22,8 +26,14 @@ public class UserService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand
     public List<User> getUserByIds(List<Integer> ids){
         User[] users = restTemplate.getForObject("http://provider/user/{1}", User[].class, StringUtils.join(ids, ","));
         return Arrays.asList(users);
+    }
+
+    @HystrixCollapser(batchMethod = "getUserByIds",collapserProperties = {@HystrixProperty(name="timerDelayInMilliseconds",value = "200")})
+    public Future<User> getUserById(Integer id){
+        return null;
     }
 }
