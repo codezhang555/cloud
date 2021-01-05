ef89494416c3034720a63493abf3ed674a895fe5
codezhang <729912142@qq.com>
2021-01-05 21:56:08 +0800

    添加micrometer开启服务监控

--- micrometer/src/main/java/cn/itz/cloud/MicrometerApplication.java
+++ micrometer/src/main/java/cn/itz/cloud/MicrometerApplication.java
@@ -3,6 +3,10 @@ package cn.itz.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 微服务由于服务数量多，所以出故障的概率很大，
+ * 官方建议监控工具使用Micrometer
+ */
 @SpringBootApplication
 public class MicrometerApplication {
 
